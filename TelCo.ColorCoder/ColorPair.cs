7b59d61a828f5d62dcfab9fa292df9d7a416693a
Ananya Chandramohan <77439415+AnanyaC97@users.noreply.github.com>
2021-02-22 19:23:30 +0530

    Add files via upload

---
+++ TelCo.ColorCoder/ColorPair.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelCo.ColorCoder
+{
+    class ColorPair
+    {
+
+        private static Color[] colorMapMajor;
+
+        private static Color[] colorMapMinor;
+
+        internal class ColorPair
+        {
+            internal Color majorColor;
+            internal Color minorColor;
+            public override string ToString()
+            {
+                return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+            }
+        }
+        /// <summary>
+        /// Static constructor required to initialize static variable
+        /// </summary>
+        static ColorPair()
+        {
+            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
+            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+
+    }
+}
